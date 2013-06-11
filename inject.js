575b1bce58d863a61e391b040e53ada9436c359b
reedfim <reedfim@gmail.com>
2013-06-11 18:43:18 +0900

    베타버전 완료

--- inject.js
+++ inject.js
@@ -34,43 +34,80 @@ function beautify(source) {
 
 	return output;
 }
+function trimStr( str ) {
+	var lre = /\s*((\S+\s*)*)/;
+	var rre = /((\s*\S+)*)\s*/;
+
+	return str.replace(lre, "$1").replace(rre, "$1");
+}
 
 
-function indentTrigger() {
+var wi = {};
+wi.backupTag = [];//{newcode}, {code}
+wi.sources = null;
+
+
+function indentTrigger( flagArr ) {
+	var textArea = $('#markupTextarea');
+
+	for (var i = 1, j=0, len = wi.sources.length; i < len; i += 2, j++) {
+		if(flagArr[j]){
+			wi.sources[i] = wi.backupTag[i - 1] + '\n' + trimStr(beautify(wi.sources[i])) + '\n' + wi.backupTag[i];	
+		}else{
+			wi.sources[i] = wi.backupTag[i - 1] + '\n' + trimStr(wi.sources[i]) + '\n' + wi.backupTag[i];	
+		}
+	}
+
+	textArea.val(wi.sources.join('')); //결과 반영
+	
+	//초기화
+	wi.sources = null;
+	wi.backupTag = [];
+}
+
+
+function extractCodeBlock(){
 	var textArea = $('#markupTextarea');
 
 	if (textArea.length >= 1) {
 		var markupText = textArea.val(),
 			delim = '##removeline##',
-			backupTag = [], //{newcode}, {code}
-			sources = markupText.replace(/({\s*(new)?code.*})/gi, function(match, $1, $2) {
-				//console.log(match, $1, $2);
-				backupTag.push($1);
-				return delim;
-			}).split(delim);
+			retArr = [];
+
+		wi.sources = markupText.replace(/({\s*(new)?code.*})/gi, function(match, $1, $2) {
+			wi.backupTag.push($1);
+			return delim;
+		}).split(delim);
 
 
-		if (sources.length % 2 === 1) {
-			for (var i = 1, len = sources.length; i < len; i += 2) {
-				//console.log(backupTag[i - 1] + '\n' + beautify(sources[i]) + '\n' + backupTag[i]);
-				sources[i] = backupTag[i - 1] + '\n' + beautify(sources[i]) + '\n' + backupTag[i];
+		if (wi.sources.length % 2 === 1) {
+			for (var i = 1, len = wi.sources.length; i < len; i += 2) {
+				retArr.push(wi.sources[i].replace(/\t/g,'&nbsp;&nbsp;&nbsp;&nbsp;').replace(/\n/g,'<br>'));
 			}
 
-			textArea.val(sources.join('')); //결과 반영
+			return retArr;
 
 		} else {
 			alert('어디선가 태그가 잘못되었습니다.');
+			return null;
 		}
 	} else {
 		alert('편집 모드가 아닙니다.');
+		return null;
 	}
 }
 
 //익스텐션 아이콘을 선택할때마다의 모드 체인지
 chrome.runtime && chrome.runtime.onMessage.addListener(
 	function(request, sender, sendResponse){
-		if(request.action){
-			indentTrigger();
+		if(request.action === 'GET'){
+			sendResponse({
+				res : extractCodeBlock()
+			});
+
+		}else if(request.action === 'INDENT'){
+			var arr = request.indexArr;
+			indentTrigger(arr);
 		}
 	}
 );
--- popup.js
+++ popup.js
@@ -1,8 +1,65 @@
-document.getElementById('actionIndentBtn').addEventListener('click', function(){ 
+document.getElementById('codePanel').innerHTML = '';
+
+var getBtn = document.getElementById('actionGetBtn');
+var indentBtn = document.getElementById('actionIndentBtn');
+
+getBtn.addEventListener('click', function(){ 
 	chrome.tabs.getSelected(null, function(tab) {
   		chrome.tabs.sendMessage(tab.id, {
-				action : true
-			}, function(response) {} //이 코드 부분에서는 상위의 sendResponse가 반응하지 않는다.
+				action : 'GET'
+			}, function(response) {
+				var res = response.res;
+				if(res != null){
+					getAndShow(res);
+				}else{
+					alert('여기가 아닌가 봐요~~');
+				}
+			} 
 		);
 	});
 });
+
+indentBtn.addEventListener('click', function(){ 
+	chrome.tabs.getSelected(null, function(tab) {
+  		chrome.tabs.sendMessage(tab.id, {
+				action : 'INDENT',
+				indexArr : getCheckedBox()
+			}, function(response) {} 
+		);
+		
+		getBtn.style.display = 'block';
+		indentBtn.style.display = 'none';
+		document.getElementById('codePanel').innerHTML = '';
+	});
+});
+
+function getAndShow( res ){
+	var panel = document.getElementById('codePanel');
+	panel.innerHTML = '';
+
+	var ul = document.createElement('ul');
+	for(var i=0, len=res.length; i<len; i++){
+		var li = document.createElement('li');
+		li.innerHTML = '<div class="alert alert-success"><input type="checkbox" value="'+i+'">'+decodeURIComponent(res[i])+'</div>';
+		ul.appendChild(li);
+	}
+
+	panel.appendChild(ul);
+
+	getBtn.style.display = 'none';
+	indentBtn.style.display = 'block';
+}
+
+function getCheckedBox(){
+	var retArr = [];
+	var inputs = document.querySelectorAll('#codePanel input');
+	for(var i=0,len = inputs.length; i<len; i++){
+		if(inputs[i].checked){
+			retArr.push(true);
+		}else{
+			retArr.push(false);
+		}
+	}
+
+	return retArr;
+}
